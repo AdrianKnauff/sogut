433d977eeac71a53f07262b00b138b91cfcf2732
Adrian Knauff <adrian.knauff@bwsolution.de>
2017-09-25 08:44:28 +0200

    Config-value-setter

--- src/Core/Config.php
+++ src/Core/Config.php
@@ -8,15 +8,15 @@ class Config
     private $values;
     private static $instance;
     private static $configFile;
-    
+
     private function __construct()
     {
         $this->baseUrl = "";
         $this->values = [];
-        $this->loadConfigFile();
+//        $this->loadConfigFile();
     }
 
-    public static function getInstance() : Config
+    public static function getInstance(): Config
     {
         if (empty(self::$instance)) {
             self::$instance = new Config();
@@ -24,7 +24,7 @@ class Config
         return self::$instance;
     }
 
-    public function __get(string $keyName) : string
+    public function __get(string $keyName): string
     {
         return $this->values[$keyName];
     }
@@ -34,8 +34,15 @@ class Config
         $configs = parse_ini_file(self::$configFile, TRUE);
         $this->values = array_pop($configs);
     }
-    
-    public static function setConfigFile($configFile) {
+
+    public static function setConfigFile($configFile)
+    {
         self::$configFile = $configFile;
     }
+
+    public static function __set(String $keyName, String $value)
+    {
+        $this->values[$keyName] = $value;
+    }
+
 }
